ea2c4877067e991806443b88d73a07d96d6adc4a
dylan <C24486954@myTUDublin.ie>
2025-11-16 13:39:25 +0000

    stuff

---
+++ Source/Slime/Private/Characters/SlimeAnim.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/SlimeAnim.h"
+
+#include "Characters/SlimeCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void USlimeAnim::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Slime = Cast<ASlimeCharacter>(TryGetPawnOwner());
+	if (Slime)
+	{
+		SlimeMovement = Slime->GetCharacterMovement();
+	}
+}
+
+void USlimeAnim::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!Slime || !SlimeMovement) return;
+	
+	GroundSpeed = SlimeMovement->Velocity.Size2D();
+	bIsFalling = SlimeMovement->IsFalling();
+}
